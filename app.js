b6c8be274124ea78e12ca21bc87dac34c3dc3d2f
Ralitsa Popova <ralicaterzieva90@gmail.com>
2024-03-16 18:23:25 +0200

    Add new endpoints and fix bugs

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({extended: true}))
+
 app.get('/', (req, res) => {
     res.render('home');
 });
@@ -28,6 +30,16 @@ app.get('/campgrounds', async (req, res) => {
     const campgrounds = await CampGround.find({})
     res.render('campgrounds/index', { campgrounds })
 });
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+})
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new CampGround(req.body);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+})
 
 app.get('/campgrounds/:id', async (req, res) => {
     const camp = await CampGround.findById(req.params.id)
@@ -39,6 +51,7 @@ app.get('/makecampground', async (req, res) => {
     await camp.save();
     res.send(camp);
 });
+
 
 app.listen(3000, () => {
     console.log('Serving on PORT 3000!');
