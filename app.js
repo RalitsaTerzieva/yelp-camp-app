b63fd47a918ddd549c230e6bfcef11573493d50c
Ralitsa Popova <ralicaterzieva90@gmail.com>
2024-03-16 19:05:21 +0200

    Add morgan logger

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const CampGround = require('./models/campground');
+const morgan = require('morgan');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -23,6 +24,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
+app.use(morgan('tiny'))
 
 app.get('/', (req, res) => {
     res.render('home');
