0c214356917fff03407cfcf24bb086a144fe29cd
Ralitsa Popova <ralicaterzieva90@gmail.com>
2024-03-16 18:55:24 +0200

    Add eidt and delete logic

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const CampGround = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -20,7 +21,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -45,6 +47,22 @@ app.get('/campgrounds/:id', async (req, res) => {
     const camp = await CampGround.findById(req.params.id)
     res.render('campgrounds/show', { camp })
 });
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await CampGround.findById(req.params.id)
+    res.render('campgrounds/edit', { campground })
+})
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const campground = await CampGround.findByIdAndUpdate(req.params.id, req.body)
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params
+    await CampGround.findByIdAndDelete(id)
+    res.redirect(`/campgrounds`)
+})
 
 app.get('/makecampground', async (req, res) => {
     const camp = new CampGround({title: 'Yellow Camp', price: 400, description: 'Best camp in the Rila mountain!'})
