acc4cdd8ed2259eecfc26f283d6d70686dbf8a34
Ralitsa Popova <ralicaterzieva90@gmail.com>
2024-03-23 19:16:55 +0200

    Use ExpressError

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 const CampGround = require('./models/campground');
 const morgan = require('morgan');
 
@@ -58,6 +59,7 @@ app.get('/campgrounds/new', (req, res) => {
 })
 
 app.post('/campgrounds', catchAsync(async (req, res, next) => {
+    if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400)
     const campground = new CampGround(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`) 
@@ -90,8 +92,14 @@ app.get('/makecampground', catchAsync(async (req, res) => {
     res.send(camp);
 }));
 
+app.all('*', (err, req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+})
+
 app.use((err, req, res, next) => {
-    res.status(404).send('Not Found!')
+    const { statusCode = 500 } = err;
+    if(!err.message) err.message = 'Oh no, something get wrong!'
+    res.status(statusCode).render('error', { err });
 })
 
 
