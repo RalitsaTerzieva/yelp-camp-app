0858e395b720339a3283162937c4109f468d1514
Ralitsa Popova <ralicaterzieva90@gmail.com>
2024-03-24 12:11:53 +0200

    Add Joi library

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const Joi = require('joi');
 const methodOverride = require('method-override');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
@@ -59,7 +60,20 @@ app.get('/campgrounds/new', (req, res) => {
 })
 
 app.post('/campgrounds', catchAsync(async (req, res, next) => {
-    if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400)
+    // if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400)
+    const campgroundSchema = Joi.object({
+        campground: Joi.object({
+            title: Joi.string().required(),
+            location: Joi.string().required(),
+            price: Joi.number().required().min(0).positive()
+        }).required()
+    })
+
+    const { error } = campgroundSchema.validate(req.body)
+    if(error) {
+        const message = error.details.map(msg => msg.message).join(',')
+        throw new ExpressError(message, 400)
+    }
     const campground = new CampGround(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`) 
