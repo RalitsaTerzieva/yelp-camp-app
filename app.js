652066476cfd08fd645a6ba0cf04b061bad28838
Ralitsa Popova <ralicaterzieva90@gmail.com>
2024-03-31 19:54:48 +0300

    Add public folder

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(morgan('dev'))
 app.use((req,res,next) => {
     console.log(req.method, req.path)
