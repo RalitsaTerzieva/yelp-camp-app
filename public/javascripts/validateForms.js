417849adc8e06fe725de98b2c22e122b6fb3df0e
Ralitsa Popova <ralicaterzieva90@gmail.com>
2024-04-14 16:54:51 +0300

    Customize file input field

--- public/javascripts/validateForms.js
+++ public/javascripts/validateForms.js
@@ -1,5 +1,7 @@
 (() => {
     'use strict'
+
+    bsCustomFileInput.init()
   
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     const forms = document.querySelectorAll('.needs-validation')
